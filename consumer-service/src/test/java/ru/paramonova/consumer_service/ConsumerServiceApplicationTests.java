7860d54e63cf8e7944fa9f4d196868be57cdd059
Anna <annaparamonova18122004@gmail.com>
2025-07-01 20:15:28 +0400

    initial commit

---
+++ consumer-service/src/test/java/ru/paramonova/consumer_service/ConsumerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.paramonova.consumer_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
